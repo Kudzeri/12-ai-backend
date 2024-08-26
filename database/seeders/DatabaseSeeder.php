8683344e63bc66060d22807450628bdaba402f4f
Kudzeri <kudzeri@gmail.com>
2024-08-26 16:40:49 +0500

    upd: seeding func to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Advertisement;
+use App\Models\Category;
 use App\Models\ContactInfo;
 use App\Models\Post;
 use App\Models\User;
@@ -10,17 +11,28 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        Category::truncate();
+        User::truncate();
+        Advertisement::truncate();
+        Post::truncate();
+        ContactInfo::truncate();
+
         $users = User::factory()->count(10)->create();
+
+        $categories = Category::factory()->count(10)->create();
 
         $advertisements = Advertisement::factory()
             ->count(10)
             ->create([
                 'user_id' => $users->random()->id,
+                'category_id' => $categories->random()->id,
+                'subcategory_id' => $categories->random()->id
             ]);
 
         foreach ($advertisements as $advertisement) {
